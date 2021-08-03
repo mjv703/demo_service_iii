c4a8f4a2b9ffccf323a46a11b0b234f6a5da73ec
mehdi <mjv703@yahoo.com>
2021-08-03 21:09:44 +0430

    UserInfoDTO is added in register methode

--- src/main/java/com/medicai/pillpal/domain/UserInfo.java
+++ src/main/java/com/medicai/pillpal/domain/UserInfo.java
@@ -19,8 +19,8 @@ public class UserInfo implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    //@SequenceGenerator(name = "sequenceGenerator")
     private Long id;
 
     @Column(name = "user_code", unique = true)
